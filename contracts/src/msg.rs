256881d0ee47f23e1f55bd5214319d996a0b2038
Laura Weindorf <lauraweindorf@gmail.com>
2021-04-17 19:09:38 -0400

    added create/query proposal

--- contracts/src/msg.rs
+++ contracts/src/msg.rs
@@ -1,26 +1,31 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use cosmwasm_std::{CanonicalAddr};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InitMsg {
-    pub message: String,
-}
+pub struct InitMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Reset { message: String },
+    CreateProposal { name: String },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
-    GetMessage {},
+    GetProposal { proposal_id: u64 },
 }
 
-// We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct MessageResponse {
-    pub message: String,
+pub struct ProposalResponse {
+    pub id: u64,
+    pub name: String,
+    pub creator: CanonicalAddr,
+    pub status: u64,
+    pub num_votes: u64,
+    pub tally_yes: u64,
+    pub tally_no: u64,
 }
+
